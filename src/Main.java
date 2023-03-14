a147c64964d6990623b36c730c6485e23fca6e8d
Perkles <gabriel_felipe_trabalho@hotmail.com>
2023-03-14 10:01:54 -0300

    Classes: Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //apartir da palavra new que agente instancia o objeto:
+        Curso curso = new Curso();
+        curso.setTitulo("curso java");
+        curso.setDescricao("descriçao do curso java");
+        curso.setCargaHoraria(8);
+
+        //criando outro curso(curso 2):
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descriçao do curso js");
+        curso2.setCargaHoraria(4);
+
+        //criando e instanciando a classe mentoria:
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
